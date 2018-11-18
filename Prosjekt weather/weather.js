1b4fbf1affe2cb356716232d4ba3c6626f52a06e
tarjeitax <tarjeitax@hotmail.com>
2018-11-18 20:25:10 +0100

    Fjernet div vi ikke bruker

--- Prosjekt weather/weather.js
+++ Prosjekt weather/weather.js
@@ -1,97 +1,34 @@
-<<<<<<< HEAD
 //Melding
 let weatherApp = new Vue({
   el: "#app",
   data: {
     currentTemp: '',
-    test: '',
+    currentWindSpd: '',
     city: ''
   },
 
   methods: {
-    methodTest() {
-      this.test = "test3";
-    },
-    customWeather(city) {
-      this.test = "test2";
+    getWeather(city) {
       this.city = city;
-      let url = "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&APPID=2509c1ec40c32caf78e39a525a287619";
-      axios
-        .get(url)
-        .then(response => {
-          this.currentTemp = Math.round(response.data.main.temp - 273.15);
-        })
-        .catch(error => {
-          console.log(error);
-        });
+
+      var i;
+      for (i = 0; i < 1; i++) {
+        let url = "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&APPID=2509c1ec40c32caf78e39a525a287619";
+        axios
+          .get(url)
+          .then(response => {
+            this.currentTemp = Math.round(response.data.main.temp - 273.15);
+            this.currentWindSpd = response.data.wind.speed;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
     },
 
-    getWeather() {
-      this.test = "test";
-      let city = "Oslo";
-      this.city = city;
-      let url = "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&APPID=2509c1ec40c32caf78e39a525a287619";
-      axios
-        .get(url)
-        .then(response => {
-          this.currentTemp = Math.round(response.data.main.temp - 273.15);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
   },
   beforeMount() {
     //this.customWeather("Oslo");
   },
 
 })
-=======
-//Melding
-let weatherApp = new Vue({
-  el: "#app",
-  data: {
-    currentTemp: '',
-    test: '',
-    city: ''
-  },
-
-  methods: {
-    methodTest() {
-      this.test = "test3";
-    },
-    customWeather(city) {
-      this.test = "test2";
-      this.city = city;
-      let url = "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&APPID=2509c1ec40c32caf78e39a525a287619";
-      axios
-        .get(url)
-        .then(response => {
-          this.currentTemp = Math.round(response.data.main.temp - 273.15);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-
-    getWeather() {
-      this.test = "test";
-      let city = "Oslo";
-      this.city = city;
-      let url = "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&APPID=2509c1ec40c32caf78e39a525a287619";
-      axios
-        .get(url)
-        .then(response => {
-          this.currentTemp = Math.round(response.data.main.temp - 273.15);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-  },
-  beforeMount() {
-    //this.customWeather("Oslo");
-  },
-
-})
->>>>>>> 3e2971eb9a910731839a1c63a4b8851a1b41d837
