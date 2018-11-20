a687a2f0f40587334e7a0f5ccc0226f4f66b7bf1
tarjeitax <tarjeitax@hotmail.com>
2018-11-20 14:13:13 +0100

    La til array for URL'er

--- Prosjekt weather/weather.js
+++ Prosjekt weather/weather.js
@@ -11,19 +11,25 @@ let weatherApp = new Vue({
     getWeather(city) {
       this.city = city;
 
+      var urlArray = new Array(
+        "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&APPID=2509c1ec40c32caf78e39a525a287619",
+        "http://api.openweathermap.org/data/2.5/weather?q=Olso&APPID=2509c1ec40c32caf78e39a525a287619");
+
       var i;
       for (i = 0; i < 1; i++) {
-        let url = "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&APPID=2509c1ec40c32caf78e39a525a287619";
+        let url = urlArray[i];
         axios
           .get(url)
           .then(response => {
             this.currentTemp = Math.round(response.data.main.temp - 273.15);
+            //tempVar = Math.round(response.data.main.temp - 273.15);
             this.currentWindSpd = response.data.wind.speed;
           })
           .catch(error => {
             console.log(error);
           });
       }
+      //this.currentTemp = tempVar;
     },
 
   },
