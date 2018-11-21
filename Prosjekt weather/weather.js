0705ac6deddad947a3571a17f74facaf080b4ece
tarjeitax <tarjeitax@hotmail.com>
2018-11-21 13:59:31 +0100

    La til flere API'er, jobbet med kombinering av resultat

--- Prosjekt weather/weather.js
+++ Prosjekt weather/weather.js
@@ -2,36 +2,66 @@
 let weatherApp = new Vue({
   el: "#app",
   data: {
-    currentTemp: '',
-    currentWindSpd: '',
+    currentTemp: 0,
+    currentWindSpd: 0,
     city: ''
   },
 
   methods: {
     getWeather(city) {
       this.city = city;
+      this.currentTemp = 0;
+      this.getWeather1(city);
+      this.getWeather2(city);
+      this.getWeather3(city);
+      this.getAverage();
+    },
 
-      var urlArray = new Array(
-        "http://api.openweathermap.org/data/2.5/weather?q= " + city + "&APPID=2509c1ec40c32caf78e39a525a287619",
-        "http://api.openweathermap.org/data/2.5/weather?q=Olso&APPID=2509c1ec40c32caf78e39a525a287619");
-
+    getWeather1(city) {
+      var urlArray = new Array("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=2509c1ec40c32caf78e39a525a287619");
       var i;
       for (i = 0; i < 1; i++) {
-        let url = urlArray[i];
+        let url = urlArray[0]
         axios
           .get(url)
           .then(response => {
-            this.currentTemp = Math.round(response.data.main.temp - 273.15);
-            //tempVar = Math.round(response.data.main.temp - 273.15);
-            this.currentWindSpd = response.data.wind.speed;
+            this.currentTemp = this.currentTemp + response.data.main.temp
+            //this.currentWindSpd = response.data.wind.speed;
           })
           .catch(error => {
             console.log(error);
           });
       }
-      //this.currentTemp = tempVar;
     },
 
+    getWeather2(city) {
+      let url = "https://api.weatherbit.io/v2.0/current?city=" + city + "&key=3ac7161f99364282867da9e035189d75";
+      axios
+        .get(url)
+        .then(response => {
+          this.currentTemp = this.currentTemp + response.data.data[0].temp;
+          //this.currentWindSpd = response.data.wind.speed;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
+    getWeather3(city) {
+      let url = "https://api.apixu.com/v1/current.json?key=c42cf85cc5c14e369e9111602182111&q=" + city + "";
+      axios
+      .get(url)
+      .then(response => {
+        this.currentTemp = this.currentTemp + response.data.current.temp_c;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+
+    getAverage() {
+          this.currentTemp = this.currentTemp + 100;
+    },
   },
   beforeMount() {
     //this.customWeather("Oslo");
