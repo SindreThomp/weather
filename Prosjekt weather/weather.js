b6e058d4dedae8ce1d61b65e7b00fb00725e9176
tarjeitax <tarjeitax@hotmail.com>
2018-11-23 23:10:54 +0100

    La til delay så man bare ser resultat, la til vindhastighet

--- Prosjekt weather/weather.js
+++ Prosjekt weather/weather.js
@@ -1,9 +1,10 @@
-//Melding
 let weatherApp = new Vue({
   el: "#app",
   data: {
     currentTemp: 0,
+    averageTemp: 0,
     currentWindSpd: 0,
+    averageWindSpd: 0,
     city: ''
   },
 
@@ -11,27 +12,24 @@ let weatherApp = new Vue({
     getWeather(city) {
       this.city = city;
       this.currentTemp = 0;
+      this.currentWindSpd = 0;
       this.getWeather1(city);
       this.getWeather2(city);
       this.getWeather3(city);
-      this.getAverage();
+      setTimeout(() => this.getAverage(), 1000);
     },
 
     getWeather1(city) {
-      var urlArray = new Array("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=2509c1ec40c32caf78e39a525a287619");
-      var i;
-      for (i = 0; i < 1; i++) {
-        let url = urlArray[0]
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=2509c1ec40c32caf78e39a525a287619";
         axios
           .get(url)
           .then(response => {
-            this.currentTemp = this.currentTemp + response.data.main.temp
-            //this.currentWindSpd = response.data.wind.speed;
+            this.currentTemp = this.currentTemp + response.data.main.temp;
+            this.currentWindSpd = this.currentWindSpd + response.data.wind.speed;
           })
           .catch(error => {
             console.log(error);
           });
-      }
     },
 
     getWeather2(city) {
@@ -40,7 +38,7 @@ let weatherApp = new Vue({
         .get(url)
         .then(response => {
           this.currentTemp = this.currentTemp + response.data.data[0].temp;
-          //this.currentWindSpd = response.data.wind.speed;
+          this.currentWindSpd = this.currentWindSpd + response.data.data[0].wind_spd;
         })
         .catch(error => {
           console.log(error);
@@ -50,17 +48,21 @@ let weatherApp = new Vue({
     getWeather3(city) {
       let url = "https://api.apixu.com/v1/current.json?key=c42cf85cc5c14e369e9111602182111&q=" + city + "";
       axios
-      .get(url)
-      .then(response => {
-        this.currentTemp = this.currentTemp + response.data.current.temp_c;
-      })
-      .catch(error => {
-        console.log(error);
-      });
+        .get(url)
+        .then(response => {
+          this.currentTemp = this.currentTemp + response.data.current.temp_c;
+          this.currentWindSpd = this.currentWindSpd + (response.data.current.wind_kph / 3.6);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     },
 
     getAverage() {
-          this.currentTemp = this.currentTemp + 100;
+      this.currentTemp = (this.currentTemp / 3).toFixed(2);
+      this.averageTemp = this.currentTemp;
+      this.currentWindSpd = (this.currentWindSpd / 2).toFixed(2);
+      this.averageWindSpd = this.currentWindSpd;
     },
   },
   beforeMount() {
